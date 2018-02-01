3562a67e055fb4007bfedadb0ece220d8120ce4a
Remi Pierot <rfpierot91@gmail.com>
2018-02-01 10:59:13 +0100

    Make everything static in ControlManager

--- ResidentGestalt/Assets/Scripts/Control/ControlManager.cs
+++ ResidentGestalt/Assets/Scripts/Control/ControlManager.cs
@@ -5,30 +5,19 @@ namespace Control
 {
 	public class ControlManager : MonoBehaviour
 	{
-		public static ControlManager Instance
-		{
-			get;
-			private set;
-		}
+		private static Controller _Controller;
+		private static Keyboard _Keyboard;
 
-		private Controller _Controller;
-		private Keyboard _Keyboard;
-
-		public static bool Up { get { return Instance._Controller.Up || Instance._Keyboard.Up; } }
-		public static bool Down { get { return Instance._Controller.Down || Instance._Keyboard.Down; } }
-		public static bool Left { get { return Instance._Controller.Left || Instance._Keyboard.Left; } }
-		public static bool Right { get { return Instance._Controller.Right || Instance._Keyboard.Right; } }
+		public static bool Up { get { return _Controller.Up || _Keyboard.Up; } }
+		public static bool Down { get { return _Controller.Down || _Keyboard.Down; } }
+		public static bool Left { get { return _Controller.Left || _Keyboard.Left; } }
+		public static bool Right { get { return _Controller.Right || _Keyboard.Right; } }
 		public static bool InputPressed { get { return Up || Down || Left || Right; } }
 
 		private void Start()
 		{
-			if (Instance == null)
-				Instance = this;
-			else
-				return;
-
-			Instance._Controller = Instance.gameObject.AddComponent<Controller>();
-			Instance._Keyboard = Instance.gameObject.AddComponent<Keyboard>();
+			_Controller = gameObject.AddComponent<Controller>();
+			_Keyboard = gameObject.AddComponent<Keyboard>();
 		}
 	}
 }
