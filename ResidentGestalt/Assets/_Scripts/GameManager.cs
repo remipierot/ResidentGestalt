20f443c291abf2b65513c942e07af2143d5f46af
LAPTOP\Axel <deretaxel@gmail.com>
2018-02-01 09:20:44 +0100

    Push ma scène + création de certains dossier pour organiser + création de truc pour tests

---
+++ ResidentGestalt/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GameManager : MonoBehaviour {
+
+
+	public List<GameObject> Dates;
+	public List<NavMeshAgent> NavMeshes;
+
+	public Transform tableDate;
+	public Transform outside;
+
+	public int currentDate = 0;
+
+	public int test = 0;
+
+	// Use this for initialization
+	void Start () {
+
+		currentDate = 0;
+		dateGoTo (currentDate, tableDate.position);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		dateGoTo (currentDate, tableDate.position);
+		if (NavMeshes [currentDate].isStopped)
+			test = 1;
+		else
+			test = 2;
+
+
+	}
+
+
+	void dateGoTo(int indexDate, Vector3 position)
+	{
+		NavMeshes [indexDate].SetDestination (position);
+	}
+}
