6ea867d2b75f1b6a279015d35f47aa6379279203
Remi Pierot <rfpierot91@gmail.com>
2018-02-01 09:34:46 +0100

    Add namespace + TMP dll

---
+++ ResidentGestalt/Assets/Scripts/Control/InputTest.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using Control.Type;
+
+namespace Control
+{
+	public class InputTest : MonoBehaviour
+	{
+		private Controls controller;
+		private Controls keyboard;
+
+		private void Start()
+		{
+			controller = gameObject.AddComponent<Controller>();
+			keyboard = gameObject.AddComponent<Keyboard>();
+		}
+
+		private void Update()
+		{
+			if (controller.InputPressed)
+			{
+				if (controller.Up)
+				{
+					Debug.LogError("CONTROLLER - Up");
+				}
+
+				if (controller.Down)
+				{
+					Debug.LogError("CONTROLLER - Down");
+				}
+
+				if (controller.Left)
+				{
+					Debug.LogError("CONTROLLER - Left");
+				}
+
+				if (controller.Right)
+				{
+					Debug.LogError("CONTROLLER - Right");
+				}
+			}
+
+			if (keyboard.InputPressed)
+			{
+				if (keyboard.Up)
+				{
+					Debug.LogError("KEYBOARD - Up");
+				}
+
+				if (keyboard.Down)
+				{
+					Debug.LogError("KEYBOARD - Down");
+				}
+
+				if (keyboard.Left)
+				{
+					Debug.LogError("KEYBOARD - Left");
+				}
+
+				if (keyboard.Right)
+				{
+					Debug.LogError("KEYBOARD - Right");
+				}
+			}
+		}
+	}
+}
---
+++ ResidentGestalt/Assets/Scripts/Control/Type/Controller.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Control.Type
+{
+	public class Controller : Controls
+	{
+		void Update()
+		{
+			Up = Input.GetButtonDown("Up");
+			Down = Input.GetButtonDown("Down");
+			Left = Input.GetButtonDown("Left");
+			Right = Input.GetButtonDown("Right");
+		}
+	}
+}
