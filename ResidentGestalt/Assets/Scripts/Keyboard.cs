d323c9e4de217d8dc19574f1ed1642ca547e1dc4
Remi Pierot <rfpierot91@gmail.com>
2018-02-01 09:10:37 +0100

    First version for controls

---
+++ ResidentGestalt/Assets/Scripts/Keyboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Keyboard : Controls
+{	
+	void Update()
+	{
+		Up = Input.GetKeyDown(KeyCode.UpArrow);
+		Down = Input.GetKeyDown(KeyCode.DownArrow);
+		Left = Input.GetKeyDown(KeyCode.LeftArrow);
+		Right = Input.GetKeyDown(KeyCode.RightArrow);
+	}
+}
