d4b63d42e4007436828567e4aa61d9fae589dff5
Adrien <meuseuneu@hotmail.fr>
2018-02-03 15:28:19 +0100

    no message

---
+++ ResidentGestalt/Assets/_Scenes/Scene_Adrien/Assets_Adrien/3D/BlockSambaDancer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSambaDancer : MonoBehaviour {
+
+	public Transform real_pos;
+	public Transform samba_hips;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void OnAnimatorMove()
+	{
+		Animator animator = GetComponent<Animator>(); 
+
+		if (animator)
+		{
+
+			samba_hips.position = new Vector3 (real_pos.position.x, samba_hips.position.y, real_pos.position.z);
+		}
+	}
+
+	void LateUpdate () {
+		samba_hips.position = new Vector3 (real_pos.position.x, samba_hips.position.y, real_pos.position.z);
+	}
+}
